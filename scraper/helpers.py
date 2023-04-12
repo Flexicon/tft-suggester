2e18385c1596a134608362b61fba6534a9939007
Mike Repec <mikerepec@gmail.com>
2023-04-12 16:26:48 +0200

    Disable /dev/shm usage in chrome webdriver

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -14,6 +14,7 @@ class ScraperWebDriver:
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(5)
 
