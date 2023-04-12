8ed2d78decdee184defcb1ef6df562d7b497e8f0
Mike Repec <mikerepec@gmail.com>
2023-04-12 16:30:16 +0200

    Mount /dev/shm instead of disabling it all together

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -14,7 +14,6 @@ class ScraperWebDriver:
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(5)
 
