200cab1f2fa796fabffec6e58216dbed0b72d525
Mike Repec <mikerepec@gmail.com>
2023-12-18 19:31:32 +0100

    Add some flags to try to make chrome webdriver not timeout as much
    see: https://stackoverflow.com/a/52340526/3112167

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -11,8 +11,14 @@ from common.models import Champion
 class ScraperWebDriver:
     def __init__(self) -> None:
         chrome_options = webdriver.ChromeOptions()
+        # try anything to make ChromeDriver more consistent: https://stackoverflow.com/a/52340526/3112167
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("enable-automation")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-browser-side-navigation")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.set_page_load_timeout(30)
         self.driver.implicitly_wait(5)
