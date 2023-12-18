f3d8eed8ada84b2da0c59424cf7a057497892008
Mike Repec <mikerepec@gmail.com>
2023-12-18 18:22:11 +0100

    Remove disable-gpu flag from web driver

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -13,7 +13,6 @@ class ScraperWebDriver:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(5)
 
