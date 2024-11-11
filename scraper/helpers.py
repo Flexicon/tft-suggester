3ff03c21be75b27e9d7792f541c31479f8441426
Mike Repec <mikerepec@gmail.com>
2024-11-11 11:34:34 +0100

    fix: reduce scraper page load timeout and increase retry attempts

--- scraper/helpers.py
+++ scraper/helpers.py
@@ -14,7 +14,7 @@ class ScraperWebDriver:
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.set_page_load_timeout(15)
+        self.driver.set_page_load_timeout(10)
         self.driver.implicitly_wait(5)
 
     def fetch_content_html(self, url: str, *, selector: str = '.main') -> str:
