95047671c6054cf8a2ec74bf564dd66020ff2d0a
Mike Repeć <mikerepec@gmail.com>
2025-05-27 17:08:33 +0200

    feat: Setup CI in GitHub Actions and reformat codebase (#10)

--- common/db/db.py
+++ common/db/db.py
@@ -8,14 +8,16 @@ class DB:
     _connection = None
     _database = None
 
-    def connect(self, connection_url: str = None) -> 'DB':
+    def connect(self, connection_url: str = None) -> "DB":
         if connection_url is None:
-            connection_url = os.getenv('MONGODB_URI')
+            connection_url = os.getenv("MONGODB_URI")
 
-        self._connection = MongoClient(connection_url,
-                                       connectTimeoutMS=30000,
-                                       socketTimeoutMS=None,
-                                       socketKeepAlive=True)
+        self._connection = MongoClient(
+            connection_url,
+            connectTimeoutMS=30000,
+            socketTimeoutMS=None,
+            socketKeepAlive=True,
+        )
         self._database = self._connection.get_database()
         return self
 
@@ -24,14 +26,16 @@ class DB:
 
     def _get_collection(self, name: str):
         if self._database is None:
-            raise ConnectionError('Database not connected. Make sure to run connect() first.')
+            raise ConnectionError(
+                "Database not connected. Make sure to run connect() first."
+            )
         return self._database.get_collection(name)
 
     def get_comps_collection(self) -> Collection:
-        return self._get_collection('comps')
+        return self._get_collection("comps")
 
     def get_champions_collection(self) -> Collection:
-        return self._get_collection('champions')
+        return self._get_collection("champions")
 
     def get_items_collection(self) -> Collection:
-        return self._get_collection('items')
+        return self._get_collection("items")
--- tests/cdragon/test_items.py
+++ tests/cdragon/test_items.py
@@ -6,13 +6,13 @@ import requests_cache
 import responses
 
 from cdragon.base import BASE_URL
-from cdragon.items import ITEMS_URL, Item, get_items, _parse_item_name
+from cdragon.items import ITEMS_URL, Color, Item, get_items, _parse_item_name
 
 
 @pytest.fixture(autouse=True)
 def clear_cache():
     """Setup and clear the cdragon api cache before and after every test."""
-    session = requests_cache.CachedSession('cdragon_cache', expire_after=0)
+    session = requests_cache.CachedSession("cdragon_cache", expire_after=0)
     session.cache.clear()
     yield
     session.cache.clear()
@@ -33,22 +33,21 @@ def test_get_items_not_found():
 
 @responses.activate
 def test_get_items_ok():
-    responses.add(responses.GET, ITEMS_URL,
-                  json=_dummy_items_response(), status=200)
+    responses.add(responses.GET, ITEMS_URL, json=_dummy_items_response(), status=200)
     expected_results = [
         Item(
             guid="fe3a63e6-cc62-4299-a52a-d65755cf6cff",
             name="Blade of the Ruined King",
             id=28,
-            color={"R": 186, "B": 255, "G": 46, "A": 255},
-            loadoutsIcon=f"{BASE_URL}/assets/maps/particles/tft/tft_item_bladeoftheruinedking.png"
+            color=Color(R=186, B=255, G=46, A=255),
+            loadoutsIcon=f"{BASE_URL}/assets/maps/particles/tft/tft_item_bladeoftheruinedking.png",
         ),
         Item(
             guid="a56e667b-36c6-4f81-98f7-e79188ac1a5e",
             name="Berserker's Axe",
             id=89,
-            color={"R": 0, "B": 0, "G": 0, "A": 255},
-            loadoutsIcon=f"{BASE_URL}/assets/maps/particles/tft/tft_item_berserkeraxe.png"
+            color=Color(R=0, B=0, G=0, A=255),
+            loadoutsIcon=f"{BASE_URL}/assets/maps/particles/tft/tft_item_berserkeraxe.png",
         ),
     ]
 
@@ -57,10 +56,13 @@ def test_get_items_ok():
     assert results == expected_results
 
 
-@pytest.mark.parametrize("name, expected", [
-    ("TFT_item_name_BladeOfTheRuinedKing", "Blade Of The Ruined King"),
-    ("tft_item_name_Set5Abomination_RadiantSpat", "Set 5 Abomination Radiant Spat"),
-])
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("TFT_item_name_BladeOfTheRuinedKing", "Blade Of The Ruined King"),
+        ("tft_item_name_Set5Abomination_RadiantSpat", "Set 5 Abomination Radiant Spat"),
+    ],
+)
 def test_parse_item_name(name, expected):
     got = _parse_item_name(name)
     assert got == expected, "result did not match expected value"
@@ -73,13 +75,13 @@ def _dummy_items_response() -> List[dict]:
             "name": "Blade of the Ruined King",
             "id": 28,
             "color": {"R": 186, "B": 255, "G": 46, "A": 255},
-            "loadoutsIcon": "/lol-game-data/assets/ASSETS/Maps/Particles/TFT/TFT_Item_BladeOfTheRuinedKing.png"
+            "loadoutsIcon": "/lol-game-data/assets/ASSETS/Maps/Particles/TFT/TFT_Item_BladeOfTheRuinedKing.png",
         },
         {
             "guid": "a56e667b-36c6-4f81-98f7-e79188ac1a5e",
             "name": "Berserker's Axe",
             "id": 89,
             "color": {"R": 0, "B": 0, "G": 0, "A": 255},
-            "loadoutsIcon": "/lol-game-data/assets/ASSETS/Maps/Particles/TFT/TFT_Item_BerserkerAxe.png"
-        }
+            "loadoutsIcon": "/lol-game-data/assets/ASSETS/Maps/Particles/TFT/TFT_Item_BerserkerAxe.png",
+        },
     ]
