3eff06dc59d4fdb626c7953f8fb794603eca7061
Mike Repec <mikerepec@gmail.com>
2025-12-06 14:00:47 +0100

    fix(items): migrate to cdragon as data source for items

--- scraper/scrape_items.py
+++ scraper/scrape_items.py
@@ -1,79 +1,20 @@
-import requests
-
 from typing import List
 
-from bs4 import BeautifulSoup, Tag
 from pymongo.collection import Collection
 
-from common.models import CompositeItem, Item
+from common.models import CompositeItem
 from common.db import DB
-from scraper.helpers import require_tag_by_class
-
-ScrapeURL = r"https://www.mobafire.com/teamfight-tactics/items-cheatsheet"
-Selector = ".mobile-items .item"
-
-NameReplacements = {
-    "A D M I N": "A.D.M.I.N.",
-    "Zzrot": "Zz'Rot",
-}
-NameApostrophes = [
-    "Archangels",
-    "Bulwarks",
-    "Deaths",
-    "Dragons",
-    "Giants",
-    "Guinsoos",
-    "Nashors",
-    "Protectors",
-    "Rabadons",
-    "Randuins",
-    "Rascals",
-    "Runaans",
-    "Steraks",
-    "Tacticians",
-    "Thiefs",
-    "Titans",
-    "Warmogs",
-    "Zekes",
-]
+from cdragon.items import get_composite_items
 
 
 def scrape_items() -> List[CompositeItem]:
-    print(f"Fetching items from: {ScrapeURL}")
-    res = requests.get(ScrapeURL)
-    res.raise_for_status()
+    """Fetch composite TFT items from Community Dragon API.
 
-    items = BeautifulSoup(res.text, "html.parser").select(Selector)
-    return list(map(_build_composite_item, items))
-
-
-def _build_composite_item(div: Tag) -> CompositeItem:
-    image = str(require_tag_by_class(div, "final")["src"])
-
-    return CompositeItem(
-        name=_name_from_image_url(image),
-        image=image,
-        components=[
-            _build_item_from_component(c) for c in div.find_all(class_="component")
-        ],
-    )
-
-
-def _build_item_from_component(img: Tag) -> Item:
-    url = str(img["src"])
-    return Item(name=_name_from_image_url(url), image=url)
-
-
-def _name_from_image_url(url: str) -> str:
-    name = url.split("/")[-1].split(".")[0].replace("-", " ").title()
-
-    for k, v in NameReplacements.items():
-        name = name.replace(k, v)
-
-    for v in NameApostrophes:
-        name = name.replace(v, v[:-1] + "'s")
-
-    return name
+    Returns a list of CompositeItem objects with their components.
+    Names are already properly formatted from the official data source.
+    """
+    print("Fetching items from Community Dragon API...")
+    return get_composite_items()
 
 
 def scrape_and_persist(collection: Collection):
